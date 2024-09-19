ae529303691de40256d1dc1b4c6f1b81945767bb
Tawanfs <86216706+Tawanfs@users.noreply.github.com>
2024-09-18 22:33:45 -0300

    Finished

--- Pages/Contact Us.py
+++ Pages/Contact Us.py
@@ -9,7 +9,7 @@ with st.form(key='email_form'):
     user_message = st.text_area("Describe", key="text")
     user_submited = st.form_submit_button("Submit")
     if user_submited:
-        message = f"{user_email}\n Topic: {user_choice}\n {user_message}"
+        message = f"{user_email}\nTopic: {user_choice}\n{user_message}"
         funcs.send_mail(message)
         st.info("The Email Was Sent Successfully")
 
